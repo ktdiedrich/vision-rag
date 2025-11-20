f45cb11c112f83e2beacdf6cc26a910382179818
Karl Diedrich <ktdiedrich@gmail.com>
2025-11-19 21:48:51 -0800

    Retore default dataset

--- vision_rag/config.py
+++ vision_rag/config.py
@@ -7,7 +7,7 @@ from typing import Dict, Any
 # Environment variable configuration
 CLIP_MODEL_NAME = os.getenv("VISION_RAG_CLIP_MODEL", "clip-ViT-B-32")
 DINO_MODEL_NAME = os.getenv("VISION_RAG_DINO_MODEL", "facebook/dino-vits8")
-MEDMNIST_DATASET = os.getenv("VISION_RAG_DATASET", "PneumoniaMNIST")
+MEDMNIST_DATASET = os.getenv("VISION_RAG_DATASET", "OrganSMNIST")
 COLLECTION_NAME = os.getenv("VISION_RAG_COLLECTION_NAME", "vision_rag")
 # Default encoder type for images. Options: 'clip' or 'dino' (default)
 # DINO tends to produce more distinct clusters in embedding space and is the
